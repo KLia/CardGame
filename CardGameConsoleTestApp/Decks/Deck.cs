076f77e7ea547d8f5449ae0789a4fa646f813ae5
KLia <keithlia@gmail.com>
2016-03-13 23:27:39 +0100

    Added Decks folder and Deck class

---
+++ CardGameConsoleTestApp/Decks/Deck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CardGameConsoleTestApp.Cards;
+
+namespace CardGameConsoleTestApp.Decks
+{
+    public class Deck
+    {
+        private IList<Card> Cards;
+        public string Name;
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+        }
+
+        public Deck(IList<Card> cards)
+        {
+            Cards = cards;
+        }
+
+    }
+}
