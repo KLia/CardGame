b5ed198fb7eb0d8a4c2bc3226fce5a9f6a8653a3
Keith Lia <keithlia@gmail.com>
2016-09-19 22:25:40 +0200

    Added CardTrigger class

--- CardGameConsoleTestApp/Model/Cards/ValueObjects/CardTrigger.cs
+++ CardGameConsoleTestApp/Model/Cards/ValueObjects/CardTrigger.cs
@@ -1,7 +1,12 @@
-﻿namespace CardGameConsoleTestApp.Model.Cards.ValueObjects
+﻿using System.Collections.Generic;
+
+namespace CardGameConsoleTestApp.Model.Cards.ValueObjects
 {
     public class CardTrigger
     {
-
+        public TriggerType Type { get; set; }
+        public string MethodClass { get; set; }
+        public string MethodName { get; set; }
+        public List<TriggerMethodParam> MethodParams { get; set; }
     }
 }
